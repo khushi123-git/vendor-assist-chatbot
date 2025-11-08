9d9cddf916a995585972dbdc88526a7ff9f52fd8
SanikaShaha <sanikashaha51@gmail.com>
2025-11-08 10:44:25 +0530

    Update supabase URL to use JWT token
    Replaced supabase URL with a JWT token.

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 
-const supabaseUrl = 'https://biibpkmrulenfvamhxsm.supabase.co'
+const supabaseUrl = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpaWJwa21ydWxlbmZ2YW1oeHNtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MzUzNTczNywiZXhwIjoyMDY5MTExNzM3fQ.uCxBDNrvfdP-7flOjHPjazy3NRxTtpAQcCl4L6-Zhts'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpaWJwa21ydWxlbmZ2YW1oeHNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM1MzU3MzcsImV4cCI6MjA2OTExMTczN30.dR5cIlXpe39TKo7KexoEKhR3TbPu-8x7GKcochLNoT0'
-
 export const supabase = createClient(supabaseUrl, supabaseKey)
